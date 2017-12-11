bd8c66d900429bf0f5896e5714741066ed4730cf
188descon <blondeandcat@gmail.com>
2017-12-11 14:29:48 -0500

    Create Content.js

---
+++ Content.js
@@ -0,0 +1,34 @@
+function init()
+chrome.browserAction.onClicked.addListener(funtion (tab){
+});
+document.getElementById("button").onClick = function getLastOne();
+    chrome.storage.sync.clear();
+chrome.commands.onCommand.addListener(function (onClick){
+call()
+});
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab)
+{
+localStorage["tabTitle-" + tabId] = tabUrl;
+if (tab.title != null)
+    localStorage["tabTitle-"+tabId] = tabTitle;
+if (tab.title !=null)
+    localStorage["tabTitle-"+tabId] = tabUrl;
+});
+
+chrome.tabs.onReplaced.addListener(newTabId, oldTabId) {
+chrome.tabs.getNewTabId, function (tab){
+removedUrl = tab:url;
+});
+});
+chrome.tabs.onRemoved.addListener(function (tabID, removeInfo) {
+console.log("***onRemoved***" + tabId);
+if (typeof removedTabId !="undefined")
+{ lastRemovedId = removedTabId;}
+removedTabId = tabId;
+console.log("lastRemoved" + lastRemovedId + "recently removed" + removeTabId);
+   optimise();
+function getLastOne()
+{
+var returnString = <html><body>;
+for (i = localStorage["closeCount"]-i; 1 >=0; i--)
+{
